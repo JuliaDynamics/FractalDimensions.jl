ab63fc157d6739e90e37b9581c7f19e42a07b007
George Datseris <datseris.george@gmail.com>
2023-01-22 13:41:41 +0100

    update molteno

--- src/entropy_based/generalized_dim.jl
+++ src/entropy_based/generalized_dim.jl
@@ -2,7 +2,7 @@ using ComplexityMeasures: Renyi, ValueHistogram, entropy
 export generalized_dim
 
 """
-    generalized_dim(A::AbstractDataset [, sizes]; q = 1, base = MathConstants.e) -> Δ_q
+    generalized_dim(A::AbstractDataset [, sizes]; q = 1, base = 2) -> Δ_q
 
 Return the `q` order generalized dimension of the dataset `A`,
 by calculating  its histogram-based entropy for each `ε ∈ sizes`.
@@ -17,7 +17,7 @@ The returned dimension is approximated by the
 versus the box size `ε`, where `ε ∈ sizes`:
 
 ```math
-H_q \\sim -\\Delta_q\\log(\\varepsilon)
+H_q \\approx -\\Delta_q\\log_{b}(\\varepsilon)
 ```
 
 ``H_q`` is calculated using `entropy`, `Renyi(; base, q)` and the `ValueHistogram`
@@ -41,9 +41,10 @@ Calling this function performs a lot of automated steps:
 
 By doing these steps one by one yourself, you can adjust the keyword arguments
 given to each of these function calls, refining the accuracy of the result.
+The source code of this function is only 3 lines of code.
 """
 function generalized_dim(data::AbstractDataset, sizes = estimate_boxsizes(data);
-        base = Base.MathConstants.e, q = 1.0
+        base = 2, q = 1.0
     )
     H = [entropy(Renyi(; q, base), ValueHistogram(ε), data) for ε ∈ sizes]
     x = -log.(base, sizes)
