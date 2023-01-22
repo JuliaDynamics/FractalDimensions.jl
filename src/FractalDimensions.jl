732235d19f94b69dd54f505a6f91491760beaf75
George Datseris <datseris.george@gmail.com>
2023-01-22 12:57:46 +0100

    re-organize structure

--- src/FractalDimensions.jl
+++ src/FractalDimensions.jl
@@ -10,14 +10,19 @@ end FractalDimensions
 using Reexport
 @reexport using StateSpaceSets
 
-include("includes/linear_regions.jl")
-include("includes/generalized_dim.jl")
-include("includes/correlationsum_vanilla.jl")
-include("includes/correlationsum_boxassisted.jl")
-include("includes/correlationsum_fixedmass.jl")
-include("includes/molteno.jl")
-include("includes/kaplanyorke.jl")
-include("includes/takens_best_estimate.jl")
-include("includes/higuchi.jl")
+
+include("linear_regions.jl")
+
+include("entropy_based/generalized_dim.jl")
+include("entropy_based/molteno.jl")
+
+include("corrsum_based/correlationsum_vanilla.jl")
+include("corrsum_based/correlationsum_boxassisted.jl")
+include("corrsum_based/correlationsum_fixedmass.jl")
+include("corrsum_based/takens_best_estimate.jl")
+
+include("timeseries_roughness/higuchi.jl")
+
+include("misc/kaplanyorke.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,7 @@ using FractalDimensions
 defaultname(file) = uppercasefirst(replace(splitext(basename(file))[1], '_' => ' '))
 testfile(file, testname=defaultname(file)) = @testset "$testname" begin; include(file); end
 @testset "FractalDimensions" begin
-    # include("multiscale/multiscale.jl")
+    include("entropydim.jl")
+    include("correlationdim.jl")
+    include("higuchi.jl")
 end
