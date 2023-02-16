65ccd1fa8fcc767ea19ab5bb9582db295f74ea11
Datseris <datseris.george@gmail.com>
2023-02-16 16:53:02 +0000

    finish tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using FractalDimensions
 
 defaultname(file) = uppercasefirst(replace(splitext(basename(file))[1], '_' => ' '))
 testfile(file, testname=defaultname(file)) = @testset "$testname" begin; include(file); end
+
 @testset "FractalDimensions" begin
     include("entropydim.jl")
     include("correlationdim.jl")
