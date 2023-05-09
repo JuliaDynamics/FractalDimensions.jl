4abe49f1684720cc02d86aa35889950875fa0cf1
Datseris <datseris.george@gmail.com>
2023-05-09 12:04:15 +0100

    Revert "slightly increase saturation threshold"
    This reverts commit 7103be532bff4e817222ec88f03235098a0b0c75.

--- src/linear_regions.jl
+++ src/linear_regions.jl
@@ -106,14 +106,14 @@ Call [`linear_regions`](@ref) and identify and return the largest linear region
 The keywords `dxi, tol` are propagated as-is to [`linear_regions`](@ref).
 The keyword `ignore_saturation = true` ignores saturation that (sometimes) happens
 at the start and end of the curve `y(x)`, where the curve flattens.
-The keyword `sat = 0.05` decides what saturation is (while `abs(y[i]-y[i+1])<sat` we
+The keyword `sat = 0.01` decides what saturation is (while `abs(y[i]-y[i+1])<sat` we
 are in a saturation regime).
 
 The keyword `warning = true` prints a warning if the linear region is less than 1/3
 of the available x-axis.
 """
 function linear_region(x::AbstractVector, y::AbstractVector;
-    dxi::Int = 1, tol::Real = 0.2, ignore_saturation = true, warning = true, sat = 0.05)
+    dxi::Int = 1, tol::Real = 0.2, ignore_saturation = true, warning = true, sat = 0.01)
 
     isat = 0
     if ignore_saturation
