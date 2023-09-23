1f3f8521edef29d1b98928474d06db8a8a11e900
Ignacio del Amo <86604724+PythagoreanCult@users.noreply.github.com>
2023-09-23 15:16:26 +0100

    Changes requested, removed spaces from docstring and modified the function to get the second moment to use less memory

--- src/extremes_based/gev.jl
+++ src/extremes_based/gev.jl
@@ -6,14 +6,10 @@ Estimate and return the parameters `σ, μ` of a Generalized Extreme Value distr
 fit to `X`, assuming that the parameter `ξ` is 0, and that the extremal index θ
 is a known constant, and can be estimated through the function 
 extremal_index_sueveges(...).
-
 The estimators through the method of moments are given by
-
     σ = √((̄x²-̄x^2)/(π^2/6))
     μ = ̄x - σ(log(θ) + γ)
-
 where γ is the constant of Euler-Mascheroni.
-
 """
 
 function estimate_gev_parameters(X, θ)
@@ -31,7 +27,6 @@ export estimate_gev_parameters
 
 Estimate and return the scale parameter σ of a Generalized Extreme Value distribution
 fit to `X`, assuming that the parameter `ξ` is 0.
-
 The estimator through the method of moments is given by
 
     σ = √((̄x²-̄x^2)/(π^2/6))
